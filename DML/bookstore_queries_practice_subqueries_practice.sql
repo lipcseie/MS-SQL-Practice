5b5cc935707a169569a0734a5253d88e5a5010d1
Edit Lipcsei <lipcsei.e@gmail.com>
2024-07-30 09:01:30 +0200

    add 2 task to the subquerries practice

---
+++ DML/bookstore_queries_practice_subqueries_practice.sql
@@ -0,0 +1,8 @@
+USE BookStore
+-- Find books with a price higher than the average price of all books.
+SELECT * FROM Books
+WHERE price > (SELECT AVG(price) FROM Books);
+
+-- Retrieve book titles with a price less than the highest price of books in the "Fantasy" genre.
+SELECT Title FROM Books
+WHERE price < (SELECT MAX(price) FROM Books WHERE genre = 'Fantasy');
