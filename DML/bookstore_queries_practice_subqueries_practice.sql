dbaeb9977adc5c5728eb8fa88e1f4b3601bf0bdc
Edit Lipcsei <lipcsei.e@gmail.com>
2024-07-31 16:51:41 +0200

    add more subquerries task

--- DML/bookstore_queries_practice_subqueries_practice.sql
+++ DML/bookstore_queries_practice_subqueries_practice.sql
@@ -6,24 +6,47 @@ WHERE price > (SELECT AVG(price) FROM Books);
 -- Retrieve book titles with a price less than the highest price of books in the "Fantasy" genre.
 SELECT Title FROM Books
 WHERE price < (SELECT MAX(price) FROM Books WHERE genre = 'Fantasy');
--- Get titles of books published in the same year as "The Two Towers".
 
+-- Get titles of books published in the same year as "The Two Towers".
+SELECT Title FROM Books
+WHERE PublicationYear = (SELECT PublicationYear FROM Books WHERE Title = 'The Two Towers');
 
 -- Identify author IDs who have written more than one book.
-
+SELECT DISTINCT AuthorID FROM Books
+WHERE AuthorID IN(
+	SELECT AuthorID
+	FROM Books
+	GROUP BY AuthorID
+	HAVING COUNT(BookID) > 1
+	);
 
 -- Retrieve details of the least expensive book within each genre.
-
+SELECT * FROM Books
+WHERE Price IN(
+	SELECT MIN(price) FROM Books
+	GROUP BY Genre
+	);
 
 -- Find books priced higher than the least expensive book.
+SELECT Title FROM Books
+WHERE Price > (
+	SELECT MIN(price) FROM Books);
 
 
 -- Identify the genre with the highest average book price and list book titles from that genre.
-
+SELECT Title FROM Books
+WHERE Genre = (
+	SELECT TOP 1 Genre
+	FROM Books
+	GROUP BY Genre
+	ORDER BY AVG(Price) DESC
+	);
 
 -- List books with prices equal to the average price of "Epic" genre books.
+SELECT Title FROM Books
+WHERE Price = (
+	SELECT AVG(Price) 
+	FROM Books
+	WHERE Genre = 'Epic'
+	);
 
-
--- Find the highest-priced book(s) and list titles of books with prices greater than this highest price.
-
--- Determine the publisher ID with the most books published and list all book titles from that publisher.
