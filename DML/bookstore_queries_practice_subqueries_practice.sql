678b87f6acf14252bd36e3dcf4832232da3402c2
Edit Lipcsei <lipcsei.e@gmail.com>
2024-07-31 14:59:20 +0200

    add 8 more books subquery questions

--- DML/bookstore_queries_practice_subqueries_practice.sql
+++ DML/bookstore_queries_practice_subqueries_practice.sql
@@ -6,3 +6,24 @@ WHERE price > (SELECT AVG(price) FROM Books);
 -- Retrieve book titles with a price less than the highest price of books in the "Fantasy" genre.
 SELECT Title FROM Books
 WHERE price < (SELECT MAX(price) FROM Books WHERE genre = 'Fantasy');
+-- Get titles of books published in the same year as "The Two Towers".
+
+
+-- Identify author IDs who have written more than one book.
+
+
+-- Retrieve details of the least expensive book within each genre.
+
+
+-- Find books priced higher than the least expensive book.
+
+
+-- Identify the genre with the highest average book price and list book titles from that genre.
+
+
+-- List books with prices equal to the average price of "Epic" genre books.
+
+
+-- Find the highest-priced book(s) and list titles of books with prices greater than this highest price.
+
+-- Determine the publisher ID with the most books published and list all book titles from that publisher.
