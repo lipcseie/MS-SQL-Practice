7864a4f915a4c75dcb27b1e1b33789495f8edfed
Edit Lipcsei <lipcsei.e@gmail.com>
2024-08-13 14:02:32 +0200

    add 1 task to UNION practice

--- DML/bookstore_queries_practice_UNION_practice.sql
+++ DML/bookstore_queries_practice_UNION_practice.sql
@@ -5,3 +5,18 @@ USE BookStore;
 SELECT CONCAT(FirstName ,' ',LastName) AS Name FROM Authors 
 UNION 
 SELECT Name FROM Publishers;
+
+-- Find the names of all customers who have placed an order and all authors who have published at least one book. 
+-- Combine the results into a single list with a column named Name.
+
+-- Customers who have placed at least one order
+SELECT CONCAT(c.FirstName, ' ', c.LastName) AS Name
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+
+UNION
+
+-- Authors who have published at least one book
+SELECT CONCAT(a.FirstName, ' ', a.LastName) AS Name
+FROM Authors a
+JOIN Books b ON a.AuthorID = b.AuthorID;
